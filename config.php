db4ed52ba937fccee2c9bafa63f43b972631ad93
Wesley Maciel <weslmaciel@gmail.com>
2022-04-05 14:35:09 -0300

    started config data base

--- config.php
+++ config.php
@@ -1,7 +1,9 @@
 <?php
 
-define();
-define();
-define();
-define();
-define();
+define('DB_NAME', 'hp_crud');
+define('DB_HOST', 'localhost');
+define('DB_PORT', '3306');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+
+$dbConnection = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';port=' . DB_PORT, DB_USER, DB_PASSWORD);
