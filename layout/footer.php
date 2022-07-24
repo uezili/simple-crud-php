3ee04fa119894e347326767bb3a9d680da62b3c4
Wesley Maciel <weslmaciel@gmail.com>
2022-07-24 09:32:30 -0300

    Add new footer on main page

--- layout/footer.php
+++ layout/footer.php
@@ -3,3 +3,11 @@
 </div>
 
 </body>
+<footer class="bg-light text-center text-lg-start">
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2020 Copyright:
+        <a class="text-dark" href="https://github.com/uezili">Uézili Maciel</a>
+    </div>
+    <!-- Copyright -->
+</footer>
